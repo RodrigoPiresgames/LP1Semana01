4c3eb06b43e4914bccb2d8b799ddd3f29db75bdc
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-02 23:11:47 +0000

    Add xml documentation

---
+++ OlaMundo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    /// <summary>
+    /// This is the main class, writes Hello and Bye
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Main method 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bye, World!");
+        }
+    }
+}
