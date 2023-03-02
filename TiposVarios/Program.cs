7b1f1c202405d37dc766aa2293256d79af87f850
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-02 23:28:23 +0000

    Create TiposVarios add int type

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 9;
+            int b = 10;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+}
