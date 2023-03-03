4417540428bbdbf282cce100ed3f2674228a06ce
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-03 00:29:43 +0000

    Add new strings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -16,6 +16,24 @@ namespace VariasStrings
             Console.WriteLine(Simbol);
             Console.WriteLine(Simbol_literal);
             Console.WriteLine(barras);
+
+            string conca1 = "Hello" + funny;
+            string conca2 = "Hello " + 4;
+
+            int x = 10;
+            int z = 11;
+            int y = 12;
+
+            string xy = $"{x} plus {y} equals {x + y}";
+
+            string mega = String.Format("Eu sou o {x} e o {z}", x, z);
+
+            Console.WriteLine(conca1);
+            Console.WriteLine(conca2);
+            Console.WriteLine(xy);
+            Console.WriteLine(mega);
+            Console.WriteLine(@"funny {Simbol}", Simbol);
+
         }
     }
 }
