f515ed6d6ab7e084311b3705a95af02ce066bb9c
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-03 00:10:38 +0000

    Create VariasStrings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string funny = "Funny";
+            string Simbol = "\u01A9";
+            string Simbol_literal = @"\u01A9";
+            string barras = "\\barra\\barra\\barra";
+
+            Console.WriteLine(funny);
+            Console.WriteLine(Simbol);
+            Console.WriteLine(Simbol_literal);
+            Console.WriteLine(barras);
+        }
+    }
+}
