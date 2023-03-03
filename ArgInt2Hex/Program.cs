6c5cf12842c459c897ea262929d867c11fb7a42a
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-03 11:01:38 +0000

    Hexa test

--- ArgInt2Hex/Program.cs
+++ ArgInt2Hex/Program.cs
@@ -6,7 +6,21 @@ namespace ArgInt2Hex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for(int i = 0; i < args.Length; i++)
+            {
+
+                int intArg;
+                bool isInt = int.TryParse(args[i], out intArg);
+
+                if(isInt == true)
+                {
+                    Console.WriteLine("{0} -> É inteiro, valor hexadecimal: {0:x}", args[i]);
+                }
+                if(isInt == false)
+                {
+                    Console.WriteLine(@"{0} -> Não é inteiro", args[i]);
+                }
+            }
         }
     }
 }
--- Argumentos/Program.cs
+++ Argumentos/Program.cs
@@ -6,7 +6,7 @@ namespace Argumentos
     {
         static void Main(string[] args)
         {
-            for(int i = 0; i < args.length; i++)
+            for(int i = 0; i < args.Length; i++)
             {
                 Console.WriteLine(args[i]);
             }
