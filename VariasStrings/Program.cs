e2e4eb75c696a46ef9dad8da8aaff514fbb651ac
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-03 00:38:35 +0000

    Add new double

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -7,6 +7,9 @@ namespace VariasStrings
         static void Main(string[] args)
         {
 
+            double xx = 0.12345;
+            int ii = 18;
+
             string funny = "Funny";
             string Simbol = "\u01A9";
             string Simbol_literal = @"\u01A9";
@@ -26,13 +29,19 @@ namespace VariasStrings
 
             string xy = $"{x} plus {y} equals {x + y}";
 
-            string mega = String.Format("Eu sou o {x} e o {z}", x, z);
+            string mega = String.Format("Eu sou o {0} e o {1}", x, z);
 
             Console.WriteLine(conca1);
             Console.WriteLine(conca2);
             Console.WriteLine(xy);
             Console.WriteLine(mega);
-            Console.WriteLine(@"funny {Simbol}", Simbol);
+            Console.WriteLine(@"funny {0}", Simbol);
+
+            Console.WriteLine("{0:0.00}", xx);
+            Console.WriteLine("{0:P1}", xx);
+
+            Console.WriteLine("{0:X}", ii);
+            Console.WriteLine("{0:C}", ii);
 
         }
     }
