3420109ddcce5bb34c13224dc73ec0d5126f8e30
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-03 13:45:47 +0000

    Created ArgLongStrings

---
+++ ArgLongStrings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArgLongStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
