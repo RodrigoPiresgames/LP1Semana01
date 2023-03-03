f00ea076574e6c46cf740bf9747d638af3688d5e
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-03 10:04:34 +0000

    Created ArgInt2Hex

---
+++ ArgInt2Hex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArgInt2Hex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
