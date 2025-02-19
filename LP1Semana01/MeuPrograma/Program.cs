e5c17aab07f97802703b1406ec87a50b67ccffe7
Rodrigo <Rod5matyco@gamil.com>
2025-02-19 15:28:51 +0000

    Add comments

--- LP1Semana01/MeuPrograma/Program.cs
+++ LP1Semana01/MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Main program to write things
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Write Hello LP1! and Bye world!
+        /// </summary>
+        /// <param name="args"></param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
