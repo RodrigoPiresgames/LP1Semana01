ded7b43b266e1c7efa090f382dc30b1beb4b563b
Rodrigo <rodrigo.pires.gamedesign@gmail.com>
2025-02-21 13:26:51 +0000

    Add ints

---
+++ LP1Semana01/VariouTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace VariouTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int int_1 = 9;
+            int int_2 = 8;
+
+            Console.WriteLine($"{int_1}");
+            Console.WriteLine($"{int_2}");
+
+            
+        }
+    }
+}
