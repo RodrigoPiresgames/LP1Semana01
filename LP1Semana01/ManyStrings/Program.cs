f9fc5efae8dc29129de024cec019d3274a07f273
Rodrigo <rodrigo.pires.gamedesign@gmail.com>
2025-02-21 13:53:21 +0000

    New ManyStings

---
+++ LP1Semana01/ManyStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string s = "Um tab\t e um copyright\u03A9";
+            string d = "Um tab\t e um copyright\u03A9";
+
+            Console.WriteLine($"{s}");
+            Console.WriteLine($"{d}");
+        }
+    }
+}
