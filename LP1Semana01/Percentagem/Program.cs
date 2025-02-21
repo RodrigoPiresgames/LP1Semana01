f1252a693b825e0b4e3cebdefb272e8ab73a52f3
Rodrigo <rodrigo.pires.gamedesign@gmail.com>
2025-02-21 14:28:28 +0000

    New Percentagem

---
+++ LP1Semana01/Percentagem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Percentagem
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+            
+            Console.WriteLine("Insere 4 números reais entre 0 e 1:");
+
+            float num1 = float.Parse(Console.ReadLine());
+            float num2 = float.Parse(Console.ReadLine());
+            float num3 = float.Parse(Console.ReadLine());
+            float num4 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{num1,8:p2}");
+            Console.WriteLine($"{num2,8:p2}");
+            Console.WriteLine($"{num3,8:p2}");
+            Console.WriteLine($"{num4,8:p2}");
+        }
+    }
+}
